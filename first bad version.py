22dea43f787d79ecc3738c0aac0a7a0b2bde3636
Rohan sahu <90450416+voldemort85@users.noreply.github.com>
2026-02-18 12:04:00 +0530

    Update first bad version.py

--- first bad version.py
+++ first bad version.py
@@ -4,8 +4,8 @@ class Solution(object):
     def firstBadVersion(self, n):
         low=1
         high=n
-        while(low<n):
-            mid = low+high//2
+        while(low<high):
+            mid = low+(high)//2
             if isBadVersion(mid):
                 high=mid
             else:
