6e4524a57adea76696bad5d9266acaa9c365d66d
Rohan sahu <90450416+voldemort85@users.noreply.github.com>
2026-02-18 12:17:22 +0530

    Update first bad version.py

--- first bad version.py
+++ first bad version.py
@@ -7,7 +7,7 @@ class Solution(object):
         while(low<high):
             mid = low+(high-low)//2
             if isBadVersion(mid):
-                high=mid
+                high=mid-1
             else:
                 low=mid+1
          return low
